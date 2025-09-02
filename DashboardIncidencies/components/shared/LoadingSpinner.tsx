18f5b8978b39a938c52157c8aa499ef925b3b05f
felipsarroca <felip.sarroca@gmail.com>
2025-09-02 20:27:11 +0200

    Delete DashboardIncidencies directory

--- DashboardIncidencies/components/shared/LoadingSpinner.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-export const LoadingSpinner: React.FC = () => (
-  <div className="flex flex-col items-center justify-center p-10">
-    <div className="w-16 h-16 border-4 border-dashed rounded-full animate-spin border-brand-primary"></div>
-    <p className="mt-4 text-lg text-text-secondary font-semibold">Carregant dades...</p>
-  </div>
-);
